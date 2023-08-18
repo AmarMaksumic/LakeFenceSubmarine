bc4161add9c4127eed6ea06cc01e01c3c4911803
Amar Maksumić <amar.maksumich@gmail.com>
2023-08-18 19:53:30 -0400

    final commit

--- main.py
+++ main.py
@@ -1,244 +1,239 @@
-import cv2
-import config
-import controller_listener
-import controls
-import logging
-import start_web
-import time
-
-import _thread as thread
-import tensorflow as tf
-import ujson as json
-import websocket as ws
+import gpiozero
 import RPi.GPIO as GPIO
-
-from cameras.camera import USBCam, Camera
-from cameras import image_converter
-from cameras.video_async import VideoCaptureAsync
-
-from controls import main_controller
-
+import cv2
+import time
+import socket
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 from multiprocessing import Process
+from threading import Thread
+import _thread
+import threading
+import pickle
+import select
+import struct
+# import start_web
 
-from profiles.color_profile import ColorProfile
+def listener(client, address):
+  print("Accepted connection from: ", address)
+  with clients_lock:
+    clients.add(client)
+  try:    
+    while True:
+      data = client.recv(1024)
+      if data == '0':
+        timestamp = datetime.datetime.now().strftime("%I:%M:%S %p")
+        client.send(timestamp)
+        time.sleep(2)
+  finally:
+    with clients_lock:
+      clients.remove(client)
+      client.close()
 
-from websocket import create_connection
-
-# initiate the top level logger
-logging.basicConfig(
-  level=logging.INFO,
-  format="%(asctime)s [%(name)s] [%(levelname)-5.5s] %(message)s",
-  handlers=[
-    logging.StreamHandler()
-  ]
-)
-
-logger = logging.getLogger('app')
-# creating instance of logger object(?)
-
-def init_pins():
-  GPIO.setmode(GPIO.BCM)
-  GPIO.setup(17, GPIO.OUT) # status LED
-  GPIO.setup(18, GPIO.IN) # bump sensor 1
-  GPIO.setup(14, GPIO.IN) # bump sensor 2
-  GPIO.setup(15, GPIO.IN) # bump sensor 3
+clients = set()
+clients_lock = threading.Lock()
+host = "192.168.0.193"
 
 
-def main():  # main method defined
+port = 10016
 
-  cv2.destroyAllWindows()
+s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((host,port))
+s.listen(6)
+read_list = [s]
+th = []
 
-  init_pins()
+def input():
+  send_str = 'thank you'
+  pump_stat, arm_stat, prop_x, prop_y = 0, 0, 0, 0
+   
+  while True:
+    time.sleep(0.05)
+    try:
+      readable, writable, errored = select.select(read_list, [], [], 1)
+      for x in readable:
+        if x is s:
+          client_socket, address = s.accept()
+          read_list.append(client_socket)
+          print("[1] Connection from", address)
+        else:
+          data = x.recv(1024).decode('utf-8')
+          if len(data) > 1 and data[0] == '6':
+            # get
+            pump_stat = int(data[1])
+            arm_stat = int(data[2])
+            prop_x = int(data[3:-4])
+            prop_y = int(data[7:])
+            # print(pump_stat, " ", arm_stat, " ", prop_x, " ", prop_y)
+            if pump_stat == 0: print('Pump expelling')
+            elif pump_stat == 1: print('Pump locked')
+            elif pump_stat == 2: print('Pump compressing')
+            if arm_stat == 0: print('Arm retracting')
+            elif arm_stat == 1: print('Arm locked')
+            elif arm_stat == 2: print('Arm extending')
+            print('Motor X In: ', prop_x)
+            print('Adjusted to serial send: b\'', (prop_x-512)/4, '\'')
+            print('Motor Y In: ', prop_y)
+            print('Adjusted to serial send: b\'', (prop_y-512)/4, '\'')
+            x.sendall(send_str.encode('utf-8'))
+          else:
+            x.close()
+            read_list.remove(x)
+    except:
+      print("aop")
 
+def ultras():
+
+  print("starting ultras")
+
+  TRIG_1 = 23
+  ECHO_1 = 24
+  TRIG_2 = 27
+  ECHO_2 = 22
+
+  GPIO.setup(TRIG_1, GPIO.OUT) # bump sensor 1 trig
+  GPIO.setup(ECHO_1, GPIO.IN) # bump sensor 1 echo
+  GPIO.output(TRIG_1, False)
+  GPIO.setup(TRIG_2, GPIO.OUT) # bump sensor 2 trig
+  GPIO.setup(ECHO_2, GPIO.IN) # bump sensor 2 echo
+  GPIO.output(TRIG_2, False)
+
+  time.sleep(2)
+  while (True):
+    # client, address = s.accept()
+
+    #bump 1
+    GPIO.output(TRIG_1, True)
+
+    time.sleep(0.00001)
+
+    GPIO.output(TRIG_1, False)
+
+    pulse_start, pulse_end = 0, 0
+    crashed = time.time()
+
+    while GPIO.input(ECHO_1) == 0:
+      pulse_start = time.time()
+      if (time.time() - crashed > 0.1):
+        ultras()
+
+    crashed = time.time()
+    while GPIO.input(ECHO_1) == 1:
+      pulse_end = time.time()
+      if (time.time() - crashed > 0.1):
+        ultras()
+
+    pulse_duration = pulse_end - pulse_start
+
+    distance = pulse_duration * 17150
+
+    distance1 = round(distance, 2)
+
+    time.sleep(0.1)
+
+    #bump 2
+    GPIO.output(TRIG_2, True)
+
+    time.sleep(0.00001)
+
+    GPIO.output(TRIG_2, False)
+
+    pulse_start, pulse_end = 0, 0
+    crashed = time.time()
+
+    while GPIO.input(ECHO_2) == 0:
+      pulse_start = time.time()
+      if (time.time() - crashed > 0.1):
+        ultras()
+
+    crashed = time.time()
+    while GPIO.input(ECHO_2) == 1:
+      pulse_end = time.time()
+      if (time.time() - crashed > 0.1):
+        ultras()
+
+    pulse_duration = pulse_end - pulse_start
+
+    distance = pulse_duration * 17150
+
+    distance2 = round(distance, 2)
+
+    time.sleep(0.1)
+
+    print("BUMP1: ", distance1, "cm ===> BUMP2: ", distance2, "cm")
+    send_str = "BUMP1: " + str(distance1) + "cm ===> BUMP2: " + str(distance2) + "cm"
+    try:
+      readable, writable, errored = select.select(read_list, [], [], 1)
+      for x in readable:
+        if x is s:
+          client_socket, address = s.accept()
+          read_list.append(client_socket)
+          print("[2] Connection from", address)
+        else:
+          data = x.recv(1024).decode('utf-8')
+          if data == '0':
+            x.sendall(send_str.encode('utf-8'))
+          else:
+            x.close()
+            read_list.remove(x)
+    except:
+      print("woop")
+  s.close()
+
+def cam():
+
+  print("running camera")
   camera = PiCamera()
   camera.rotation = 180
   camera.resolution = (640, 480)
-  camera.awb_mode = 'fluorescent'
-  camera.awb_gains = 4
-  camera.exposure_mode = 'off'
+  # camera.awb_mode = 'fluorescent'
+  # camera.awb_gains = 4
+  # camera.exposure_mode = 'off'
   rawCapture = PiRGBArray(camera, size=(640, 480))
-
-  time.sleep(5)
-
-  # wide_camera_ws = create_connection("ws://localhost:5805/wide_camera/ws")
-  # far_camera_ws = create_connection("ws://localhost:5805/far_camera/ws")
-  # processed_ws = create_connection("ws://localhost:5805/processed/ws")
-  # calibration_ws = create_connection("ws://localhost:5805/calibration/ws")
-  # tracking_ws = create_connection("ws://localhost:5805/tracking/ws")
-  camera_ws = create_connection("ws://localhost:5805/camera/ws")
-  controller_listener.start("ws://localhost:5805/dashboard/ws")
-
-  logger.info('starting main loop ')
-  frame_cnt = 0
-  
+  time.sleep(0.1)
   for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     test_image = frame.array
-    frame_cnt += 1
-
-    cam_jpg = image_converter.convert_to_jpg(test_image)
-    camera_ws.send_binary(cam_jpg)
-
-    cv2.waitKey(0)
-
-  while (True):
-
-    tracking_data = []
-    ml_data = []
-
-    frame_cnt += 1
-    print(frame_cnt)
-    frame_cnt_str = str(frame_cnt)
-    frame_cnt_str = frame_cnt_str.zfill(4)
-    print(frame_cnt_str + '.jpg')
-
-    if main_controller.enable_camera:
-
-      if not main_controller.enable_read_image and not cap.isOpened():
-        print('opening camera')
-        if main_controller.enable_dual_camera:
-          farCam.open(config.far_video_source_number)
-        wideCam.open(config.wide_video_source_number)
-        # if the cap is not already open, do so
-
-      if main_controller.enable_read_image:
-        wide_bgr_frame = cv2.imread(frame_cnt_str + '.jpg')
-      else:
-        _, wide_bgr_frame = wideVideo.read()
-      wide_resized_frame = cvfilters.resize(wide_bgr_frame, 640, 480)
-      wide_rgb_frame = cv2.cvtColor(wide_resized_frame, cv2.COLOR_BGR2RGB)
-
-      if main_controller.enable_dual_camera:
-        _, far_bgr_frame = farVideo.read()
-        far_resized_frame = cvfilters.resize(far_bgr_frame, 640, 480)
-        far_rgb_frame = cv2.cvtColor(far_resized_frame, cv2.COLOR_BGR2RGB)
-      else:
-        far_rgb_frame = wide_rgb_frame
-
-      if main_controller.enable_camera_feed:
-        wide_jpg = image_converter.convert_to_jpg(wide_rgb_frame)
-        wide_camera_ws.send_binary(wide_jpg)
-
-        if main_controller.enable_dual_camera:
-          far_jpg = image_converter.convert_to_jpg(far_rgb_frame)
-          far_camera_ws.send_binary(far_jpg)
-
-        # camera_ws.send_binary(jpg)
-        # take rgb frame and convert it to a displayable jpg form, then send that as binary through websocket
-
-      if main_controller.enable_calibration_feed:
-        if main_controller.camera_mode == CAMERA_MODE_HEXAGON:
-          calibration_frame = far_rgb_frame.copy()
-
+    cv2.imshow('image', test_image)
+    send_str = pickle.dumps(test_image)
+    try:
+      readable, writable, errored = select.select(read_list, [], [], 1)
+      for x in readable:
+        if x is s:
+          client_socket, address = s.accept()
+          read_list.append(client_socket)
+          print("[3] Connection from", address)
         else:
-          calibration_frame = wide_rgb_frame.copy()
-        
-        
-        calibration_frame = color_calibrate.process(calibration_frame,
-                              camera_mode=main_controller.calibration.get('camera_mode',
-                                                    'RAW'),
-                              color_mode=main_controller.calibration.get('color_mode'),
-                              apply_mask=main_controller.calibration.get('apply_mask', False))
+          data = x.recv(1024).decode('utf-8')
+          if data == '1':
+            x.sendall(struct.pack("L", len(send_str)) + send_str)
+          else:
+            x.close()
+            read_list.remove(x)
+    except:
+      print("oopsies")
+    key = cv2.waitKey(1) & 0xFF
 
-        jpg = image_converter.convert_to_jpg(calibration_frame)
-        calibration_ws.send_binary(jpg)
+    rawCapture.truncate(0)
 
-      if main_controller.camera_mode == CAMERA_MODE_RAW:
+    if key == ord("q"):
+      GPIO.cleanup()
+      break
 
-        processed_frame = wide_rgb_frame
-        # Camera mode set to "raw" - takes rgb frame
+ 
+if __name__ == "__main__":
+  GPIO.cleanup()
+  GPIO.setwarnings(False)
+  GPIO.setmode(GPIO.BCM)
+  p1 = Process(target=ultras)
+  p2 = Process(target=cam)
+  p3 = Process(target=input)
 
-      elif main_controller.camera_mode == CAMERA_MODE_LOADING_BAY:
+  p1.start()
+  p2.start()
+  p3.start()
 
-        color_profile = main_controller.color_profiles[CAMERA_MODE_LOADING_BAY]
-        # Set color profile to that of "camera mode loading bay"
-      
-        ml_data = ml.predict(wide_rgb_frame, interpreter, input_details, output_details)
-
-        tracking_data = bay_tracker.process(wide_rgb_frame,
-                                            camera,
-                                            frame_cnt,
-                                            color_profile)
-        # Frame is displayed with bay tracking properties
-
-      elif main_controller.camera_mode == CAMERA_MODE_BALL:
-
-        color_profile = main_controller.color_profiles[
-          CAMERA_MODE_BALL]  # color profile set to the CAMERA MODE BALL one
-        # print("ball")
-
-        ml_data = ml.predict(wide_rgb_frame, interpreter, input_details, output_details)
-
-        tracking_data = ball_tracker2.process(wide_rgb_frame,
-                                              camera,
-                                              frame_cnt,
-                                              color_profile)
-
-        processed_frame, tracking_data = ball_tracker2.combine(wide_rgb_frame,
-                                                                tracking_data,
-                                                                ml_data,
-                                                                15)
-
-
-      elif main_controller.camera_mode == CAMERA_MODE_HEXAGON:
-
-        color_profile_hex = main_controller.color_profiles[CAMERA_MODE_HEXAGON]
-        
-        ml_data = ml.predict(far_rgb_frame, interpreter, input_details, output_details)
-
-        tracking_data_port = port_tracker.process(far_rgb_frame,
-                                              camera,
-                                              frame_cnt,
-                                              color_profile_hex)
-        
-        processed_frame, tracking_data = port_tracker.combine(far_rgb_frame,
-                                                                tracking_data_port,
-                                                                ml_data,
-                                                                15)
-      
-
-      if main_controller.enable_processing_feed:  # once we start showing our processing feed...
-
-        cv2.putText(processed_frame,
-              'Tracking Mode %s' % main_controller.camera_mode,
-              (10, 10),
-              cv2.FONT_HERSHEY_DUPLEX,
-              .4,
-              colors.BLUE,
-              1,
-              cv2.LINE_AA)
-
-        jpg = image_converter.convert_to_jpg(processed_frame)
-        processed_ws.send_binary(jpg)
-
-        # if out is not None:
-        #     out.write(frame)
-      if len(tracking_data) != 0 and main_controller.send_tracking_data:
-        # sort tracking data by closests object
-        # logger.info(tracking_data)
-        tracking_data = sorted(tracking_data, key=lambda i: i['dist'])
-        tracking_ws.send(json.dumps(dict(targets=tracking_data)))
-
-      # cv2.imshow('frame', processed_frame )
-      # cv2.waitKey(0)
-
-    else:
-      logger.info('waiting for control socket')
-      # IDLE mode
-      if not main_controller.enable_read_image and cap.isOpened():
-        print('closing camera')
-        wideCam.stop()
-        if main_controller.enable_dual_camera:
-          farCam.stop()
-      time.sleep(.3)
-
-    cv2.waitKey(0)
-
-    # if cv2.waitKey(1) & 0xFF == ord('q'):
-    #     break
-
-
-if __name__ == '__main__':
-  p = Process(target=start_web.main)
-  p.start()
-  main()
-  p.join()
+  p1.join()
+  p2.join()
+  p3.join()
+  # ultras()
